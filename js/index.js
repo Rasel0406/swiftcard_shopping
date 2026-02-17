c5b1b8b662b7542f70e54ec6fbe48e6dd994a2a4
Rasl0406 <durjoychowdhury0406@gmail.comm>
2026-02-17 18:43:29 +0600

    dynamic modal

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ const displayTrendingProducts = (products)=>{
     $${product.price}
   </p>
     <div class="card-actions justify-between">
-      <button class="btn flex-1 border rounded-lg py-2  font-bold text-xl text-gray-700 hover:bg-gray-100"><i class="fa-regular fa-eye"></i>
+      <button onclick ="openProductDetails(${product.id})" class="btn flex-1 border rounded-lg py-2  font-bold text-xl text-gray-700 hover:bg-gray-100"><i class="fa-regular fa-eye"></i>
             Details
             </button>
      <button class="btn btn-active bg-[#4e38f5] font-bold text-xl text-white rounded-lg px-10"><i class="fa-solid fa-cart-shopping"></i>
@@ -61,4 +61,57 @@ const displayTrendingProducts = (products)=>{
    
 
 }
+// product Details
+const openProductDetails = (productId) => {
+  const url =`https://fakestoreapi.com/products/${productId}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    displayProductDetails(data);
+  })
+}
+// display products details
+const displayProductDetails = (product) => {
+ const productDetails = document.getElementById('product_details');
+ productDetails.innerHTML = `
+        <div class="card bg-base-100 w-full shadow-sm">
+  <figure class="bg-gray-100 rounded-lg p-6 flex justify-center" >
+     <img
+      src="${product.image}"
+      alt="Shoes" class="h-64 object-contain pb-4" />
+  </figure>
+  <div class="flex justify-between items-center mt-4">
+      <div class="pl-3">
+         <span class="bg-blue-100 text-blue-600 text-xs px-3 py-1 rounded-full">
+      ${product.category}
+    </span>
+      </div>
+    <div class="flex items-center text-yellow-500 text-sm pr-4 ">
+      <i class="fa-solid fa-star"></i> <span class="ml-1 text-gray-600">${product.rating.rate} (${product.rating.count})</span>
+    </div>
+  </div>
+  <div class="card-body">
+    <h2 class="card-title py-2 font-bold text-xl">
+    ${product.title}
+    </h2>
+    <p class="py-2 font-semibold text-base">${product.description}</p>
+   <p class="font-bold text-lg py-1">
+    $${product.price}
+  </p>
+    <div class="card-actions justify-between">
+      <button class="btn flex-1 border rounded-lg py-2  font-bold text-xl text-gray-700 hover:bg-gray-100"><i class="fa-brands fa-amazon-pay"></i>
+            Buy Now
+            </button>
+     <button class="btn btn-active bg-[#4e38f5] font-bold text-xl text-white rounded-lg px-10"><i class="fa-solid fa-cart-shopping"></i>
+             Add
+            </button>
+    </div>
+  </div>
+</div>
+ 
+ 
+ `;
+ document.getElementById("my_modal_2").showModal();
+}
+
 trendingSection();
